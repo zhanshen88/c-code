42d1887d3733569401943432a99ad3d80d5660f4
zhanshen88 <84364539+zhanshen88@users.noreply.github.com>
2022-03-07 19:02:05 +0800

    Add files via upload

---
+++ cyclic utilization/N的素数.c
@@ -0,0 +1,26 @@
+//#include<stdio.h>
+//#include<math.h>
+//
+//int main()
+//{
+//	int n = 0;
+//	int i = 0;
+//	int j = 0;
+//	scanf("%d", &n);
+//	for (i = 2; i <= n; i++)
+//	{
+//		for (j = 2; j <= sqrt(i); j++)
+//		{
+//			if (i % j == 0)
+//			{
+//				break;
+//			}
+//		}
+//		if (j > sqrt(i))
+//		{
+//			printf("%d\n", i);
+//		}
+//
+//	}
+//	return 0;
+//}
