4c7c6a6b989215c8e79753f9414fa29026b6275a
zhanshen88 <84364539+zhanshen88@users.noreply.github.com>
2022-03-08 12:32:07 +0800

    Add files via upload

---
+++ cyclic utilization/数组插入处理.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+
+int main()
+{
+	int a[10] = { 0 };
+	int n = 0;
+	int i = 0;
+	int j = 0;
+	int temp = 0;
+	int len = sizeof(a) / sizeof(a[0]) ;
+	//a[9] = 0;
+	for (i = 0; i < len - 1; i++)
+	{
+		scanf("%d", &a[i]);
+	}
+	scanf("%d", &n);
+	for (i = 0; i < len; i++)
+	{
+		if (n > a[i] && n < a[i + 1])
+		{
+			a[len-1] = a[i+1];
+			a[i + 1] = n;
+			for (j = i + 2; j < len; j++)
+			{
+				temp = a[len-1];
+				a[len-1] = a[j];
+				a[j] = temp;
+			}
+		}
+		printf("%d\n", a[i]);
+	}
+	return 0;
+}
+ 
