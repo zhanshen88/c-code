9577b35bf933d134fe6fd5cc9a7f8ed3d1b1daee
zhanshen88 <84364539+zhanshen88@users.noreply.github.com>
2022-04-10 12:42:04 +0800

    Update 数字的逆序输出.c

--- cyclic utilization/数字的逆序输出.c
+++ cyclic utilization/数字的逆序输出.c
@@ -14,3 +14,29 @@
 //	}
 //	return 0;
 //}
+
+
+
+//void Reverse(int arr[], int sz)
+//{
+//	int left = 0;
+//	int right = sz - 1;
+//	while (left < right)
+//	{
+//		int temp = arr[right];
+//		arr[right] = arr[left];
+//		arr[left] = temp;
+//		left++;
+//		right--;
+//	}
+//}
+//
+//int main()
+//{
+//	int arr[10] = { 0 };
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	Init(arr, sz);//数组初始化
+//	Print(arr, sz);//数组打印
+//	Reverse(arr, sz);//数组转置
+//	Print(arr, sz);
+//}
